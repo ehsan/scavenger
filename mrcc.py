acc48ca339cc0daee7f39c8d4bb44f979232239a
Adam Phillabaum <adamp@payscale.com>
2017-10-03 20:54:55 -0700

    Added logging "the right way" for MRJob. Unfortunately, because of a known bug in MRJob, it causes duplicate log messages.
    Also, I got tired of pylint yelling me, so I cleaned a couple things

--- mrcc.py
+++ mrcc.py
@@ -1,15 +1,22 @@
 import gzip
 import logging
 import os.path as Path
-import boto
 import warc
+import boto
 from boto.s3.key import Key
 from gzipstream import GzipStreamFile
 from mrjob.job import MRJob
+from mrjob.util import log_to_stream
 
-log = logging.getLogger(__name__)
+# Set up logging
+# Duplicate log messages caused by: https://github.com/Yelp/mrjob/issues/1551
+LOG = logging.getLogger(__name__)
+log_to_stream(format="%(asctime)s;%(levelname)s;%(message)s")
 
 class CCJob(MRJob):
+    """
+    A simple way to run MRJob jobs on CommonCrawl Data
+    """
     def configure_options(self):
         super(CCJob, self).configure_options()
         self.pass_through_option('--runner')
@@ -22,6 +29,12 @@ class CCJob(MRJob):
         raise NotImplementedError('Process record needs to be customized')
 
     def mapper(self, _, line):
+        """
+        The Map of MapReduce
+        If you're using Hadoop or EMR, it pulls the CommonCrawl files from S3,
+        otherwise it pulls from the local filesystem. Dispatches each file to
+        `process_record`.
+        """
         # If we're on EC2 or running on a Hadoop cluster, pull files via S3
         if self.options.runner in ['emr', 'hadoop']:
             # Connect to Amazon S3 using anonymous credentials
@@ -33,7 +46,7 @@ class CCJob(MRJob):
         # If we're local, use files on the local file system
         else:
             line = Path.join(Path.abspath(Path.dirname(__file__)), line)
-            log.info('Loading local file {}'.format(line))
+            LOG.info('Loading local file {}'.format(line))
             ccfile = warc.WARCFile(fileobj=gzip.open(line))
 
         for i, record in enumerate(ccfile):
@@ -47,4 +60,9 @@ class CCJob(MRJob):
             yield key_val
 
     def reducer(self, key, value):
+        """
+        The Reduce of MapReduce
+        If you're trying to count stuff, this `reducer` will do. If you're
+        trying to do something more, you'll likely need to override this.
+        """
         yield key, sum(value)
