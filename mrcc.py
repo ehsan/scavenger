9a67e5bcc8cd13d173a14ab8d6bda9c9cf720425
Sebastian Nagel <sebastian@commoncrawl.org>
2017-10-26 16:49:00 +0200

    - adapt log format to default Hadoop log format - document combiner method - fix pylint warnings

--- mrcc.py
+++ mrcc.py
@@ -8,10 +8,11 @@ from gzipstream import GzipStreamFile
 from mrjob.job import MRJob
 from mrjob.util import log_to_stream
 
-# Set up logging
-# Duplicate log messages caused by: https://github.com/Yelp/mrjob/issues/1551
-LOG = logging.getLogger(__name__)
-log_to_stream(format="%(asctime)s;%(levelname)s;%(message)s", name=__name__)
+# Set up logging - must ensure that log_to_stream(...) is called only once
+# to avoid duplicate log messages (see https://github.com/Yelp/mrjob/issues/1551).
+LOG = logging.getLogger('CCJob')
+log_to_stream(format="%(asctime)s %(levelname)s %(name)s: %(message)s", name='CCJob')
+
 
 class CCJob(MRJob):
     """
@@ -41,28 +42,35 @@ class CCJob(MRJob):
             conn = boto.connect_s3(anon=True)
             pds = conn.get_bucket('commoncrawl')
             # Start a connection to one of the WARC files
+            LOG.info('Loading s3://commoncrawl/%s', line)
             k = Key(pds, line)
             ccfile = warc.WARCFile(fileobj=GzipStreamFile(k))
         # If we're local, use files on the local file system
         else:
             line = Path.join(Path.abspath(Path.dirname(__file__)), line)
-            LOG.info('Loading local file {}'.format(line))
+            LOG.info('Loading local file %s', line)
             ccfile = warc.WARCFile(fileobj=gzip.open(line))
 
-        for i, record in enumerate(ccfile):
+        for _i, record in enumerate(ccfile):
             for key, value in self.process_record(record):
                 yield key, value
             self.increment_counter('commoncrawl', 'processed_records', 1)
 
-    def combiner(self, key, value):
-        # use the reducer by default
-        for key_val in self.reducer(key, value):
+    def combiner(self, key, values):
+        """
+        Combiner of MapReduce
+        Default implementation just calls the reducer which does not make
+        it necessary to implement the combiner in a derived class. Only
+        if the reducer is not "associative", the combiner needs to be
+        overwritten.
+        """
+        for key_val in self.reducer(key, values):
             yield key_val
 
-    def reducer(self, key, value):
+    def reducer(self, key, values):
         """
         The Reduce of MapReduce
         If you're trying to count stuff, this `reducer` will do. If you're
         trying to do something more, you'll likely need to override this.
         """
-        yield key, sum(value)
+        yield key, sum(values)
